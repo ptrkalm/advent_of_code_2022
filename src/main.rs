aa5a72d75c8c581022062890a5b2f557e7f9b9bc
ptrkalm <pkalmuczak@gmail.com>
2022-12-10 05:34:03 +0100

    added days 1-5

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+mod day_1;
+mod day_2;
+mod day_3;
+mod day_4;
+mod day_5;
+
+fn main() {
+    println!("Day 1 - Part 1: {}", day_1::part_1());
+    println!("Day 1 - Part 2: {}", day_1::part_2());
+
+    println!("Day 2 - Part 1: {}", day_2::part_1());
+    println!("Day 2 - Part 2: {}", day_2::part_2());
+
+    println!("Day 3 - Part 1: {}", day_3::part_1());
+    println!("Day 3 - Part 2: {}", day_3::part_2());
+
+    println!("Day 4 - Part 1: {}", day_4::part_1());
+    println!("Day 4 - Part 2: {}", day_4::part_2());
+
+    println!("Day 5 - Part 1: {}", day_5::part_1());
+    println!("Day 5 - Part 2: {}", day_5::part_2());
+}
